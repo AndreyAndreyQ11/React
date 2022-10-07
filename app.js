54f3c80e056d5b6ea2381908df7507a9a941fb11
AndreyAndreyQ11 <aayy6491@gmail.com>
2022-10-07 19:42:35 +0300

    #1

---
+++ app.js
@@ -0,0 +1,12 @@
+// console.log('helloy world');
+
+const Joi = require('joi');
+const randomString = require('shortid');
+
+// const passwordSchema = Joi.string().min(3).max(11).alphanum();
+// console.log(passwordSchema.validate('213'));
+
+// console.log(randomString.generate());
+
+const a = randomString.generate();
+console.log(a);
